004f92acf49367827b43b7f6d41cfc0b290fccbe
Rubyroido <hvanstas@gmail.com>
2023-04-20 15:01:46 +0500

    feat: сверстал компоненты AboutMe и Portfolio

--- movies-explorer/src/components/Main/Main.js
+++ movies-explorer/src/components/Main/Main.js
@@ -2,8 +2,8 @@ import './Main.css';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
-// import AboutMe from '../AboutMe/AboutMe';
-// import Portfolio from '../Portfolio/Portfolio';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 function Main() {
   return (
@@ -11,8 +11,8 @@ function Main() {
       <Promo />
       <AboutProject />
       <Techs />
-      {/* <AboutMe /> */}
-      {/* <Portfolio /> */}
+      <AboutMe />
+      <Portfolio />
     </main>
   );
 }
